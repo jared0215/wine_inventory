3db00b55490d685608118db7285f0ad69aab823d
jared0215 <jared0215@hotmail.com>
2024-07-28 20:32:57 -0400

    feat: Set up MySQL database and initialize with SQLAlchemy models
    - Installed MySQL server and secured installation
    - Created 'wine_inventory' database
    - Created 'wine_user' with appropriate privileges
    - Updated Flask configuration to connect to the MySQL database
    - Installed necessary dependencies: mysql-connector-python, flask-sqlalchemy
    - Added init_db.py script to initialize the database
    - Configured Flask application to use SQLAlchemy with MySQL
    - Created database tables based on SQLAlchemy models

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,6 @@ from src.models import db
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://wine_user:password@localhost/wine_inventory'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://wine_user:Password#2233@localhost/wine_inventory'
     db.init_app(app)
     return app
