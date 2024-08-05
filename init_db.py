6a501749e5b9c5248e06a99da86997094a2341f6
jared0215 <jared0215@hotmail.com>
2024-08-05 15:12:28 -0400

    chore: Remove initial data population logic from init_db.py

--- init_db.py
+++ init_db.py
@@ -6,28 +6,8 @@ def initialize_database():
     db = Database()
     db.create_tables()
 
-    populate_initial_data(db)
-
     db.close_connection()
 
-def populate_initial_data(db):
-    cursor = db.connection.cursor()
-
-    # Populate the types table if empty
-    cursor.execute("SELECT COUNT(*) FROM types")
-    if cursor.fetchone()[0] == 0:
-        cursor.executemany("INSERT INTO types (type_name) VALUES (%s)", [('Red',), ('White',), ('Rosé',), ('Sparkling',)])
-        db.connection.commit()
-        print("Types table populated with initial data.")
-
-    # Populate the regions table if empty
-    cursor.execute("SELECT COUNT(*) FROM regions")
-    if cursor.fetchone()[0] == 0:
-        cursor.executemany("INSERT INTO regions (region_name) VALUES (%s)", [('Napa Valley',), ('Bordeaux',), ('Tuscany',), ('Barossa Valley',)])
-        db.connection.commit()
-        print("Regions table populated with initial data.")
-
-    cursor.close()
 
 if __name__ == "__main__":
     initialize_database()
